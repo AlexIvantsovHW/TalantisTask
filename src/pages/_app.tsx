d25d2bcb8363906a26232324d8a39f69cf002a8e
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-25 00:43:54 +0300

    add Head and content-security-policy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { store } from "@/app/store/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+        {/* Add other meta tags or head elements as needed */}
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
